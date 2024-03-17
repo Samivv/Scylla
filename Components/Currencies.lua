0f49a7a5e36cc2d98588e9ad2f0cf9930cc8264a
Sami Vauhkonen <n1vasa00@students.oamk.fi>
2024-03-17 18:48:26 +0200

    Rebrand

--- Components/Currencies.lua
+++ Components/Currencies.lua
@@ -1,7 +1,7 @@
 function CreateCurrencyDisplay(baseFrame, baseFrameWidth, baseFrameHeight)
     local currencyIDs = {341, 301, 2711, 101}
     local currencyDisplay = CreateFrame("Frame", "DailyToDoFrameCurrencyDisplay", baseFrame)
-    currencyDisplay:SetSize(baseFrameWidth, baseFrameHeight/1.75)
+    currencyDisplay:SetSize(baseFrameWidth, baseFrameHeight/1.5)
     currencyDisplay:SetPoint("TOP", baseFrame, "BOTTOM", 0, 0)
     local cdBG = currencyDisplay:CreateTexture(nil, "BACKGROUND")
     cdBG:SetAllPoints(currencyDisplay)
