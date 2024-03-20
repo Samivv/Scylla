865197f9b1f84eaaa1f8eb33477bf968bfad5933
Sami Vauhkonen <101732172+Samivv@users.noreply.github.com>
2024-03-20 20:38:26 +0200

    Update core.lua

--- core.lua
+++ core.lua
@@ -1,27 +1,3 @@
--- function Main()
---   local baseFrame,width,height = CreateBaseFrame()
---   local QuestList = CreateQuestList(baseFrame,width,height)
---   local dockerFrame2 = CreateDivider(QuestList, width, height,"Instance lockouts")
---   local instanceSection = CreateInstanceSection(dockerFrame2,width,height)
---   local footer = CreateFooter(instanceSection,width,height)
---   local currencyDisplay = CreateCurrencyDisplay(footer,width,height)
---   local dockerFrame4 = CreateDivider(currencyDisplay, width, height,"Reputations")
---   local reputationFrame = CreateReputationsFrame(dockerFrame4,width,height)
---   CreateSlashCommand(baseFrame)
--- end
-local baseLayout = {
-  {name = "baseFrame", parent = nil}, -- has to stay default
-  {name = "dockerFrame1", parent = "baseFrame"},
-  {name = "QuestList", parent = "dockerFrame1"},
-  {name = "dockerFrame2", parent = "QuestList"},
-  {name = "instanceSection", parent = "dockerFrame2"},
-  {name = "dockerFrame4", parent = "instanceSection"},
-  {name = "reputationFrame", parent = "dockerFrame4"},
-  {name = "footer", parent = "reputationFrame"},
-  {name = "currencyDisplay", parent = "footer"},
-}
-
-
 function Main()
   local status, err = pcall(function()
     local baseFrame,width,height = CreateBaseFrame()
